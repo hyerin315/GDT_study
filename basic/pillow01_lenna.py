0b0bbef18d0bd9737a9cbcc6128d50ad3b04a43a
hyerin315 <lhl9603@gmail.com>
2021-08-02 17:48:09 +0900

    Added TIL files : pillow module, txt files

---
+++ basic/pillow01_lenna.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageTk
+import tkinter as tk
+
+window = tk.Tk()
+canvas = tk.Canvas(window, width=500, height=500)
+canvas.pack()
+
+img = Image.open("./img_lenna/lenna.png")
+tk_img = ImageTk.PhotoImage(img)
+
+canvas.create_image(250, 250, image=tk_img)
+
+window.mainloop()
